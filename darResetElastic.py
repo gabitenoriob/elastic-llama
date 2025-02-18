5a1d6c5067d2552076b048620881ba5bda9521a7
Gabriela Batista Tenorio das Neves <gabriela.neves@cgu.gov.br>
2025-02-18 11:14:23 -0300

    fix routes

--- darResetElastic.py
+++
@@ -1,8 +0,0 @@
-from elasticsearch import Elasticsearch
-
-# Conectar ao Elasticsearch
-es = Elasticsearch("http://localhost:9200")
-
-es.options(ignore_status=[400, 404]).indices.delete(index="_all")
-
-print("Todos os índices foram excluídos!")
