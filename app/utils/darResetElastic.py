02147ca2fd21267e8d8cf14b0f04f822bc9ea4eb
Gabriela Batista Tenorio das Neves <gabriela.neves@cgu.gov.br>
2025-02-20 11:18:38 -0300

    funciona mas nao mt arquivo

--- app/utils/darResetElastic.py
+++ app/utils/darResetElastic.py
@@ -1,8 +1,9 @@
 from elasticsearch import Elasticsearch
-
+#curl -X DELETE "http://localhost:9200/nomeinidce"
+#pegar todos os indices curl -X GET "http://localhost:9200/_cat/indices?v"
+#deletar todos os indices curl -X DELETE "http://localhost:9200/_all"
 # Conectar ao Elasticsearch
 es = Elasticsearch("http://localhost:9200")
 
-es.options(ignore_status=[400, 404]).indices.delete(index="_all")
-
-print("Todos os índices foram excluídos!")
+if es.options(ignore_status=[400, 404]).indices.delete(index="_all"):
+    print("Todos os índices foram excluídos!")
